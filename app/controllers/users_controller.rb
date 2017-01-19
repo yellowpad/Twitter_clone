901f6f8e6a04bf6ff07a82ac4c6e18561f499d9b
yellowpad <Bram@Zaynes-MacBook-Pro.local>
2017-01-19 02:41:02 -0800

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,28 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
   
   def new
-  end
-  
-  def edit
-  end
-  
-  def show
-  end 
-  
-  def create
-  end
-  
-  def destroy
+    @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
