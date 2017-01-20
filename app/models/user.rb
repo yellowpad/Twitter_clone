0c1b68f804d730b151a63c3cbc77bf51fc383edd
yellowpad <you@example.com>
2017-01-20 02:08:00 -0800

    tweets with image uploadable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token
+	has_many :microposts, dependent: :destroy
+  attr_accessor :remember_token
 	before_save { self.email = email.downcase }
 	validates(:name, presence: true, length: { maximum: 50 })
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -11,7 +12,11 @@ class User < ActiveRecord::Base
     	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
    	 	BCrypt::Password.create(string, cost: cost)
-  	end
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 	 
 	# Returns a random token.
 	def User.new_token
@@ -33,5 +38,6 @@ class User < ActiveRecord::Base
   	# Forgets a user.
   	def forget
     	update_attribute(:remember_digest, nil)
-  	end    	
+  	end  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120011939) do
+ActiveRecord::Schema.define(version: 20170120090336) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "picture"
+  end
+
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
